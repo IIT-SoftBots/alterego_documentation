02b1b15beae4f257779d16bf7eb7b6125f63e76d
Luca Garello <luca.garello@iit.it>
2026-05-21 10:12:04 +0200

    fix

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -96,7 +96,7 @@ const GalleryList = [
   },
     {
     title: 'Muschio Selvaggio',
-    video: 'https://www.youtube.com/watch?v=8DBLtSx0Ido' https://www.youtube.com/embed/1JmQZO3mBEo?si=anhnLUzBDaGcKOU6&amp;start=80',
+    video: 'https://www.youtube.com/embed/1JmQZO3mBEo?si=anhnLUzBDaGcKOU6&amp;start=80',
     description: 'AlterEgo has been teleoperated from Milan during an episode of Muschio Selvaggio.',
   },
   {
